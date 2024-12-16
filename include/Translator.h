4e0b791d30600fa3244f5da6c2e1354fe6e44a70
T3nBuckS <eugesha120@mail.ru>
2024-12-16 15:22:43 +0300

    fix3

--- include/Translator.h
+++ include/Translator.h
@@ -7,32 +7,6 @@
 
 using namespace std;
 
-char invertBrackets(char ch)
-{
-	if (ch == '(') return ')';
-	else if (ch == '[') return ']';
-	else if (ch == '{') return '}';
-	return ' ';
-}
-
-bool checkBrackets(string& s)
-{
-	Stack<char> stack;
-	size_t len = s.length();
-	for (size_t i = 0; i < len; i++)
-	{
-		if (s[i] == '(' || s[i] == '{' || s[i] == '[')
-			stack.push(s[i]);
-		else if (s[i] == ')' || s[i] == '}' || s[i] == ']')
-			if (!stack.isEmpty() && s[i] == invertBrackets(stack.top()))
-				stack.pop();
-			else return 0;
-	}
-	if (stack.isEmpty())
-		return 1;
-	else return 0;
-}
-
 enum types { number, operation, open_bracket, close_bracket };
 
 class Term
